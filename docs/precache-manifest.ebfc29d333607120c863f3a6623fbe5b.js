81960a110684c519957880289e6e3cfefe8a0aab
Almos Gabor <almos.gabor@waitrose.co.uk>
2020-06-24 14:55:58 +0100

    Add docs folder for github pages

---
+++ docs/precache-manifest.ebfc29d333607120c863f3a6623fbe5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88469089e0154d634870c30e18a85d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b48c5eea566c43b248",
+    "url": "/static/css/main.be76555c.chunk.css"
+  },
+  {
+    "revision": "b3d00afd0e5293bc09fb",
+    "url": "/static/js/2.3a3dad29.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.3a3dad29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b48c5eea566c43b248",
+    "url": "/static/js/main.db41dc56.chunk.js"
+  },
+  {
+    "revision": "a8b891fea0d48bcc63e0",
+    "url": "/static/js/runtime-main.635bd3cf.js"
+  }
+]);
